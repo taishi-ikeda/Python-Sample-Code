0794b11da408eb3d69213e9f47c9ce2aea9d1e87
ikeda taishi <m.ikeda.taishi@gmail.com>
2025-08-24 19:20:48 +0900

    add py

---
+++ MonkeyHunting/MonkeyHuntingClass.py
@@ -0,0 +1,4 @@
+class MonkeyHunting:
+    def __init__(self,g_acc: float) -> None:
+        self.g_acc=g_acc
+        print("g_acc = ",g_acc)
---
+++ MonkeyHunting/main.py
@@ -0,0 +1,3 @@
+import MonkeyHuntingClass as MH
+
+MH = MH.MonkeyHunting(9.8)
