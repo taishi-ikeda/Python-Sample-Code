0777dbdbafbbc4052c60b19db58c6d5a012d05ec
taishi-ikeda <taishi@MacBook-Pro-79.local>
2025-08-24 22:09:34 +0900

    update main.py and doc

--- MonkeyHunting/main.py
+++ MonkeyHunting/main.py
@@ -3,17 +3,17 @@ import MonkeyHuntingClass as MH
 g_acc=-9.80655
 
 xb = 0.0
-yb = 0.0
+zb = 0.0
 
 xm = 1.0
 zm = 1.0
 
 v_abs=10.0
 
-MH = MH.MonkeyHunting(-9.8)
+MH = MH.MonkeyHunting(g_acc)
 
-MH.set_initial_bullet_position(0.0,0.0)
-MH.set_initial_monkey_position(1.0,2.0)
+MH.set_initial_bullet_position(xb,zb)
+MH.set_initial_monkey_position(xm,zm)
 MH.set_ground(0.0)
 [flag,theta] = MH.solve_theta(v_abs)
 if flag:
