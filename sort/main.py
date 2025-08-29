700a861542ea044442f2682489c0fdb79c4f350e
ikeda taishi <m.ikeda.taishi@gmail.com>
2025-08-29 21:39:42 +0900

    add heap sort

--- sort/main.py
+++ sort/main.py
@@ -4,9 +4,10 @@ import sort_quick as sp
 import sort_selection as ss
 import sort_insertion as si
 import sort_shell as ssh
+import sort_heap as sh
 import random
 
-arr = [random.randint(-100,100) for i in range(30)]
+arr = [random.randint(-100,100) for i in range(10)]
 print(arr)
 SortMerge = sm.merge_sort(False)
 arr_merge=SortMerge.execute(arr)
@@ -31,6 +32,10 @@ print(arr_insertion)
 SortShell = ssh.shell_sort(False)
 arr_shell = SortShell.execute(arr)
 print(arr_shell)
+
+SortHeap = sh.heap_sort(False,1000)
+arr_heap = SortHeap.execute(arr)
+print(arr_heap)
 
 
 
